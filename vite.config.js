f18b6db291dfb4a7a73f7994b8a974fb7e06f867
Timothy Alcaide <timothy.alcaide@gmail.com>
2025-05-10 02:15:09 +0200

    add github actions 2

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,8 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/main.js'),
       name: 'WaitlySnippet',
-      fileName: 'waitly-snippet',  // Should generate waitly-snippet.umd.js
+      fileName: (format) => `waitly-snippet.umd.js`,
       formats: ['umd'],
-    },
-    rollupOptions: {
-      output: {
-        file: 'dist/waitly-snippet.umd.js',  // Explicitly set the output file name
-      },
-    },
+    }
   },
 });
